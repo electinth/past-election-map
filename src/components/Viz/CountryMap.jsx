a5ce740c5499f87502a798123ef5a42d3a39cc07
thasarito <thasaritp@icloud.com>
2020-02-29 12:03:16 +0700

    render thai map

--- src/components/Viz/CountryMap.jsx
+++ src/components/Viz/CountryMap.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 import CountryGeoJson from '../../data/geojson/thailand.geo.json';
 
 const CountryMap = () => {
-	console.log(CountryGeoJson);
+	useEffect(() => {
+		const w = 500,
+			h = 1000,
+			SCALE = 1500;
+
+		const projection = d3
+			.geoMercator()
+			.translate([-2350, 650])
+			.scale([SCALE]);
+
+		const path = d3.geoPath(projection);
+
+		const $svg = d3
+			.select('#vis')
+			.attr('width', w)
+			.attr('height', h);
+		const $map = d3.select('#map');
+
+		const $path = $map
+			.selectAll('path')
+			.data(CountryGeoJson.features)
+			.enter()
+			.append('path')
+			.attr('d', path)
+			.attr('fill', 'white');
+	}, []);
 
 	return (
-		<svg>
-			<g className="thailand"></g>
+		<svg id="vis">
+			<g id="map"></g>
 		</svg>
 	);
 };
