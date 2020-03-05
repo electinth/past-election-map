90808701596cac4d7a76bb190e2d820ece5c0b40
thasarito <thasaritp@icloud.com>
2020-03-06 06:19:38 +0700

    style dropdown button

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,7 +4,10 @@ import { withRouter } from 'react-router-dom';
 const Dropdown = props => {
   return (
     <div className="dropdown--container">
-      <button className="dropdown--button">{props.children}</button>
+      <button className="dropdown--button">
+        {props.children}
+        <i className="dropdown--chevron"></i>
+      </button>
       <div className="dropdown--items"></div>
     </div>
   );
