8551f299f66ce86a16cf03e433b9fdfc8629676c
thasarito <thasaritp@icloud.com>
2020-03-14 18:16:38 +0700

    zoom using useEffect

--- src/components/Viz/D3Map.js
+++ src/components/Viz/D3Map.js
@@ -2,8 +2,12 @@ import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import * as tps from 'topojson-simplify';
 
-function D3Map(CountryTopoJson, w, h, push) {
-  let $svg, $map, electionYear;
+function D3Map(CountryTopoJson, w, h, push, initElectionYear, initProvince) {
+  console.log('Shoiuld run only once');
+  let $svg = null,
+    $map,
+    electionYear = initElectionYear,
+    province = initProvince;
   const SCALE = 2250;
 
   const projection = d3
@@ -26,16 +30,59 @@ function D3Map(CountryTopoJson, w, h, push) {
 
   const setSVG = svg => {
     $svg = svg;
+    console.log('setSVG', $svg);
     $map = $svg.select('#map');
   };
 
   const setElectionYear = year => {
     electionYear = year;
     render(electionYear);
+    console.log('set Election Year', $svg);
+  };
+
+  const setProvince = newProvince => {
+    province = newProvince;
+    console.log($svg);
+    if (province !== 'ประเทศไทย') {
+      // Reset
+      console.log('setProvince', electionYear);
+
+      const selection = {
+        type: 'FeatureCollection',
+        features: topojson
+          .feature(geo, geo.objects[electionYear])
+          .features.filter(
+            ({ properties: { province_name } }) => province_name === province
+          )
+      };
+
+      const b = path.bounds(selection);
+      const zoomScale =
+        0.75 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h);
+      const centroid = path.centroid(selection);
+      const translate = [
+        zoomScale * (-centroid[0] + w / 2),
+        zoomScale * (-centroid[1] + h / 2)
+      ];
+
+      let transform = `translate(${translate[0]}, ${translate[1]}) scale(${zoomScale})`;
+
+      $svg
+        .transition()
+        .duration(750)
+        .attr('transform', transform);
+    } else {
+      // Zoom that province
+      $svg
+        .transition()
+        .duration(750)
+        .attr('transform', '');
+    }
   };
 
   const render = year => {
-    console.log('d3', year);
+    console.log('D3Map Render Function', year);
+    electionYear = year;
     $map
       .selectAll('path.zone')
       .data(topojson.feature(geo, geo.objects[year]).features)
@@ -48,11 +95,62 @@ function D3Map(CountryTopoJson, w, h, push) {
       .attr('d', path)
       .attr('fill', d => color(d.properties.province_id % 10))
       .on('click', ({ properties: { province_name } }) =>
-        push(`/province/${province_name}`)
+        province_name === province
+          ? push('/')
+          : push(`/province/${province_name}`)
       );
+
+    // Prepare for border drawing
+    const $border = d3.select('#border');
+
+    // Country border
+    const $border_country = $border
+      .append('path')
+      .datum(
+        topojson.mesh(geo, geo.objects[year], function(a, b) {
+          return a === b;
+        })
+      )
+      .attr('class', 'country-border')
+      .attr('d', path)
+      .attr('fill', 'transparent')
+      .attr('stroke-width', '1.2')
+      .attr('stroke', 'white');
+
+    // Province borders
+    const $border_province = $border
+      .append('path')
+      .datum(
+        topojson.mesh(geo, geo.objects[year], function(a, b) {
+          return (
+            a !== b && a.properties.province_id !== b.properties.province_id
+          );
+        })
+      )
+      .attr('class', 'province-border')
+      .attr('d', path)
+      .attr('fill', 'transparent')
+      .attr('stroke-width', '0.6')
+      .attr('stroke', 'white');
+
+    // Zone borders
+    const $border_zone = $border
+      .append('path')
+      .datum(
+        topojson.mesh(geo, geo.objects[year], function(a, b) {
+          return (
+            a !== b && a.properties.province_id === b.properties.province_id
+          );
+        })
+      )
+      .attr('class', 'zone-border')
+      .attr('d', path)
+      .attr('fill', 'transparent')
+      .attr('stroke-width', '0.1')
+      .attr('stroke', 'white');
   };
 
-  return { render, setSVG, setElectionYear };
+  return { render, setSVG, setElectionYear, setProvince };
 }
 
 export default D3Map;
--- src/components/Viz/NewMap.jsx
+++ src/components/Viz/NewMap.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useContext } from 'react';
+import React, { useRef, useEffect, useContext, memo } from 'react';
 import * as d3 from 'd3';
 
 import D3Map from './D3Map';
@@ -8,21 +8,36 @@ import { withRouter } from 'react-router-dom';
 const w = 400,
   h = 700;
 
+let map;
 const NewMap = props => {
   const svgRef = useRef();
-  const { electionYear, CountryTopoJson } = useContext(MapContext);
-  const map = D3Map(CountryTopoJson, w, h, props.history.push);
+  const { province, electionYear, CountryTopoJson } = useContext(MapContext);
 
   useEffect(() => {
+    console.log('First useEffect');
+    map = D3Map(
+      CountryTopoJson,
+      w,
+      h,
+      props.history.push,
+      electionYear,
+      province
+    );
     const $svg = d3.select(svgRef.current);
     map.setSVG($svg);
     map.render(electionYear);
   }, []);
 
+  useEffect(() => {
+    if (!map) return;
+    console.log('Province Changed');
+    map.setProvince(province);
+  }, [province]);
+
   return (
     <svg id="vis" ref={svgRef} width={w} height={h}>
       <g id="map"></g>
-      <g id="border"></g>
+      <g id="border" style={{ pointerEvents: 'none' }}></g>
     </svg>
   );
 };
