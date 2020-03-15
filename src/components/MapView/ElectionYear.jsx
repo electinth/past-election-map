98bdc5667615fb72ebc09bd0cd14bb91c182d312
thasarito <thasaritp@icloud.com>
2020-03-15 11:01:04 +0700

    refactor election year menu

---
+++ src/components/MapView/ElectionYear.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useContext } from 'react';
+import { useParams, withRouter } from 'react-router-dom';
+import MapContext from '../../map/context';
+
+const TH_ELECTION_YEAR = [
+  { en: 'election-2562', th: 'ปี 2562' },
+  { en: 'election-2557', th: 'ปี 2557' },
+  { en: 'election-2554', th: 'ปี 2554' },
+  { en: 'election-2550', th: 'ปี 2550' }
+];
+
+const ElectionYear = props => {
+  const { year: paramYear } = useParams();
+  const { province, electionYear, setElectionYear } = useContext(MapContext);
+
+  useEffect(() => {
+    if (!paramYear) return;
+    setElectionYear(paramYear);
+  }, [paramYear]);
+
+  return (
+    <ul className="year-choice--list">
+      {TH_ELECTION_YEAR.map(({ en, th }) => (
+        <li
+          className={`year-choice--list-item ${en === electionYear &&
+            'year-choice--list-item__active'}`}
+          key={en}
+          onClick={() =>
+            province === 'ประเทศไทย'
+              ? props.history.push(`/${en}`)
+              : props.history.push(`/${en}/${province}`)
+          }
+        >
+          {th}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default withRouter(ElectionYear);
--- src/components/MapView/index.jsx
+++ src/components/MapView/index.jsx
@@ -1,52 +1,26 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import Dropdown from './Dropdown';
-import { Route, Switch, useParams } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { NationalLeft, NationalRight } from './NationalView';
 import { ProvincialLeft, ProvincialRight } from './ProvincialView';
 import MapContext from '../../map/context';
+import ElectionYear from './ElectionYear';
 
-const MapView = props => {
-  const { year: paramYear } = useParams();
-  const { province, electionYear, setElectionYear } = useContext(MapContext);
-  const TH_ELECTION_YEAR = [
-    { en: 'election-2562', th: 'ปี 2562' },
-    { en: 'election-2557', th: 'ปี 2557' },
-    { en: 'election-2554', th: 'ปี 2554' },
-    { en: 'election-2550', th: 'ปี 2550' }
-  ];
-
-  useEffect(() => {
-    if (!paramYear) return;
-    setElectionYear(paramYear);
-  }, [paramYear]);
+const MapView = () => {
+  const { province } = useContext(MapContext);
 
   return (
     <>
-      <div className="bar bar__left">
-        <ul className="year-choice--list">
-          {TH_ELECTION_YEAR.map(({ en, th }) => (
-            <li
-              className={`year-choice--list-item ${en === electionYear &&
-                'year-choice--list-item__active'}`}
-              key={en}
-              onClick={() =>
-                province === 'ประเทศไทย'
-                  ? props.history.push(`/${en}`)
-                  : props.history.push(`/${en}/${province}`)
-              }
-            >
-              {th}
-            </li>
-          ))}
-        </ul>
+      <aside className="bar bar__left">
+        <ElectionYear />
         <div className="bar--lower__left">
           <Switch>
             <Route path="/:year" exact component={NationalLeft} />
             <Route path="/:year/:province" component={ProvincialLeft} />
           </Switch>
         </div>
-      </div>
-      <div className="bar bar__right">
+      </aside>
+      <aside className="bar bar__right">
         <Dropdown>{province}</Dropdown>
         <div className="bar--lower__right">
           <Switch>
@@ -54,7 +28,7 @@ const MapView = props => {
             <Route path="/:year/:province" component={ProvincialRight} />
           </Switch>
         </div>
-      </div>
+      </aside>
     </>
   );
 };
