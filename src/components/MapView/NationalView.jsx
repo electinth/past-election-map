730e3b54db625be07460af0075f8579fabf63436
thasarito <thasaritp@icloud.com>
2020-03-26 20:04:22 +0700

    add candidate result in national view

--- src/components/MapView/NationalView.jsx
+++ src/components/MapView/NationalView.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import MapContext from '../../map/context';
+import _ from 'lodash';
 
 const NationalLeft = () => {
   return <></>;
@@ -22,13 +23,31 @@ const NationalRight = () => {
     );
     console.log(nationalProps);
     setNationalProps(nationalProps);
-  }, [CountryTopoJson]);
+  }, [CountryTopoJson, electionYear]);
 
   const numCandidate = nationalProps.length;
-
+  const byParty = _.groupBy(nationalProps, ({ result }) => {
+    if (!result) return 'การเลือกตั้งเป็นโมฆะ';
+    const winner = result.reduce(function(prev, current) {
+      return prev.score > current.score ? prev : current;
+    });
+    return winner.party;
+  });
+  let byPartySorted = [];
+  for (let [party, winnerResult] of Object.entries(byParty)) {
+    byPartySorted.push({ party, candidate: winnerResult.length });
+  }
+  byPartySorted.sort((a, b) => b.candidate - a.candidate);
   return (
     <>
       <h1>จำนวน {numCandidate} เขต</h1>
+      <ul>
+        {byPartySorted.map(({ party, candidate }) => (
+          <li key={party}>
+            {party}: {candidate}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
