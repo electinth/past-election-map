77681f3e421da5860cfc353d575d231d75c9b4a4
Gang (Kantapat) <gliese090@gmail.com>
2020-03-29 19:47:36 +0700

    fix-button

--- src/components/MapView/ProvincialViewDetail/ProvinceAreaCompare.jsx
+++ src/components/MapView/ProvincialViewDetail/ProvinceAreaCompare.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
-
-import FullCompareParty from './FullCompareParty.jsx';
+import { Link } from 'react-router-dom';
 
 import MapContext from '../../../map/context';
+import D3Map from '../../Viz/D3Map';
 
 const Container = styled.div`
-  height: 516px;
-  width: 351px;
+  height: 520px;
+  width: 350px;
   box-sizing: border-box;
   background-color: #ffffff;
   border-radius: 10px 6px 6px 6px;
@@ -24,8 +23,12 @@ const Title = styled.h1`
 
 const MapContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   height: 390px;
-  width: 245px;
+  width: 350px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
 `;
 
 const SeeMore = styled.button`
@@ -41,26 +44,59 @@ const SeeMore = styled.button`
   color: black;
   display: block;
   margin: auto;
+  text-declaration: none;
 `;
+
+const ContainerCard = styled.div`
+  width: 120px;
+  height: 160px;
+  margin: 5px;
+`;
+
+const TitleCard = styled.h1`
+  color: black;
+  font-family: 'The MATTER';
+  font-size: 2rem;
+`;
+
+const MapCard = styled.div`
+  width: 120px;
+  height: 140px;
+  background-color: red;
+`;
+
+const createCard = year => {
+  return (
+    <ContainerCard key={year}>
+      <TitleCard>{year}</TitleCard>
+      <MapCard>
+        <svg width={120} height={140}>
+          <g id="idMapVis"></g>
+        </svg>
+      </MapCard>
+    </ContainerCard>
+  );
+};
 
 const ProvinceAreaCompare = () => {
   const year = [2562, 2557, 2554, 2550];
-  const { province, electionYear } = useContext(MapContext);
+  const { province, electionYear, CountryTopoJson } = useContext(MapContext);
+
+  console.log('topojson');
+  console.log(CountryTopoJson);
+  useEffect(() => {});
+
   return (
     <Container>
       <Title>เปรียบเทียบ 4 ปี</Title>
       <MapContainer>
-        <Title>{province}</Title>
+        {year.map(val => {
+          return createCard(val);
+        })}
       </MapContainer>
-      <SeeMore>
-        ดูเพิ่มเติม
-        <Switch>
-          <Route
-            path="/:year/:province/compare-area/party"
-            component={FullCompareParty}
-          />
-        </Switch>
-      </SeeMore>
+      <Link to={`/compare/${province}`}>
+        <SeeMore>ดูเพิ่มเติม</SeeMore>
+      </Link>
     </Container>
   );
 };
