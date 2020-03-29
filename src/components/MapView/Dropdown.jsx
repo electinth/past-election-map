978a9bf8a94f0d76f6cdf3207b2056712128c2e8
thasarito <thasaritp@icloud.com>
2020-03-29 13:08:25 +0700

    click outside dropdown to close

--- src/components/MapView/Dropdown.jsx
+++ src/components/MapView/Dropdown.jsx
@@ -1,11 +1,15 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { withRouter } from 'react-router-dom';
 import MapContext from '../../map/context';
 
 let provinces = [];
 const Dropdown = props => {
   const { electionYear, setProvince, CountryTopoJson } = useContext(MapContext);
-  const [showItems, setShowItems] = useState(false);
+  const {
+    ref,
+    isComponentVisible: showItems,
+    setIsComponentVisible: setShowItems
+  } = useComponentVisible(false);
   useEffect(() => {
     if (CountryTopoJson.length === 0) return;
     provinces = Array.from(
@@ -19,7 +23,7 @@ const Dropdown = props => {
     provinces.unshift('ประเทศไทย');
   }, [electionYear, CountryTopoJson]);
   return (
-    <div className="dropdown--container">
+    <div className="dropdown--container" ref={ref}>
       <button
         className="dropdown--button"
         onClick={() => setShowItems(prev => !prev)}
@@ -51,3 +55,25 @@ const Dropdown = props => {
 };
 
 export default withRouter(Dropdown);
+
+function useComponentVisible(initialIsVisible) {
+  const [isComponentVisible, setIsComponentVisible] = useState(
+    initialIsVisible
+  );
+  const ref = useRef(null);
+
+  const handleClickOutside = event => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  });
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+}
