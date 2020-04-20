161f685e78d7afd4843e7ca79566b78c2a85cf0f
Dollapak Suwanpunya <dollapak@ippd.or.th>
2020-04-21 03:00:33 +0700

    for field `zone_detail_text`

--- tools/create-topojson.sh
+++ tools/create-topojson.sh
@@ -16,11 +16,11 @@ echo "Preparing thailand-election.topo.json ..."
 mapshaper geo/2562.geojson \
   -simplify 0.02 \
   -filter-islands min-vertices=4 min-area=20km2 \
-  -rename-layers 2562 \
-  -join csv/province.csv keys=province,name_th fields=* string-fields=province_id,region_id \
+  -rename-layers election-2562 \
+  -join csv/province.csv keys=province,name_th fields='*' string-fields=province_id,region_id \
   -rename-fields province_name=name_th,province_name_en=name_en,zone_id=zone_no,code_name=code_th,code_name_en=code_en,region_name=region_th \
   -each 'id=`${province_id}-${zone_id}`, pid=`${province_id}-${province_name_en.toLowerCase()}`, zone_name=`${province_name} เขต ${zone_id}`' \
-  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en \
+  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en,zone_detail_text \
   -o tmp/geojson/2562.geojson field-order=ascending
 
 
@@ -28,33 +28,33 @@ mapshaper geo/2562.geojson \
 mapshaper geo/2557.geojson \
   -simplify 0.02 \
   -filter-islands min-vertices=4 min-area=20km2 \
-  -rename-layers 2557 \
-  -join csv/province.csv keys=province,name_th fields=* string-fields=province_id,region_id \
+  -rename-layers election-2557 \
+  -join csv/province.csv keys=province,name_th fields='*' string-fields=province_id,region_id \
   -rename-fields province_name=name_th,province_name_en=name_en,zone_id=zone_no,code_name=code_th,code_name_en=code_en,region_name=region_th \
   -each 'id=`${province_id}-${zone_id}`, pid=`${province_id}-${province_name_en.toLowerCase()}`, zone_name=`${province_name} เขต ${zone_id}`' \
-  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en \
+  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en,zone_detail_text \
   -o tmp/geojson/2557.geojson field-order=ascending
 
 # B.E.2554
 mapshaper geo/2554.geojson \
   -simplify 0.02 \
   -filter-islands min-vertices=4 min-area=20km2 \
-  -rename-layers 2554 \
-  -join csv/province.csv keys=province,name_th fields=* string-fields=province_id,region_id \
+  -rename-layers election-2554 \
+  -join csv/province.csv keys=province,name_th fields='*' string-fields=province_id,region_id \
   -rename-fields province_name=name_th,province_name_en=name_en,zone_id=zone_no,code_name=code_th,code_name_en=code_en,region_name=region_th \
   -each 'id=`${province_id}-${zone_id}`, pid=`${province_id}-${province_name_en.toLowerCase()}`, zone_name=`${province_name} เขต ${zone_id}`' \
-  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en \
+  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en,zone_detail_text \
   -o tmp/geojson/2554.geojson field-order=ascending
 
 # B.E.2550
 mapshaper geo/2550.geojson \
   -simplify 0.02 \
   -filter-islands min-vertices=4 min-area=20km2 \
-  -rename-layers 2550 \
-  -join csv/province.csv keys=province,name_th fields=* string-fields=province_id,region_id \
+  -rename-layers election-2550 \
+  -join csv/province.csv keys=province,name_th fields='*' string-fields=province_id,region_id \
   -rename-fields province_name=name_th,province_name_en=name_en,zone_id=zone_no,code_name=code_th,code_name_en=code_en,region_name=region_th \
   -each 'id=`${province_id}-${zone_id}`, pid=`${province_id}-${province_name_en.toLowerCase()}`, zone_name=`${province_name} เขต ${zone_id}`' \
-  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en \
+  -filter-fields id,province_id,zone_id,zone_name,province_name,province_name_en,region_id,region_name,code_name,code_name_en,zone_detail_text \
   -o tmp/geojson/2550.geojson field-order=ascending
 
 
